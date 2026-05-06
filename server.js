92f3811dda45357e3067c742d4f931743c91edfc
Danaidea-up <hawezdana3@gmail.com>
2026-05-06 22:16:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,14 @@ const { Server } = require("socket.io");
 
 const app = express();
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const server = http.createServer(app);
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
